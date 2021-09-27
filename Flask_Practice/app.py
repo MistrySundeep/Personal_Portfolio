d07fefb81568329b86d6e1b84b68db44302232e0
Sundeep Mistry <mistrysundeep@gmail.com>
2021-09-27 21:02:30 +0100

    Set up folders for website
    Moving to creating a first draft page

---
+++ Flask_Practice/app.py
@@ -0,0 +1,16 @@
+# This is mandatory for Flask projects
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+
+# .route() is used to the app which URL should call the associated function
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+# Runs the application
+if __name__ == "__main__":
+    app.run()
+
