d1ebe430f491664b6a90a30548a104b227319965
Sundeep Mistry <mistrysundeep@gmail.com>
2021-09-28 00:40:36 +0100

    Playing with Flask-Bootstrap

--- Flask_Practice/app.py
+++ Flask_Practice/app.py
@@ -1,16 +1,18 @@
 # This is mandatory for Flask projects
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder="app")
+Bootstrap(app)
 
 
 # .route() is used to the app which URL should call the associated function
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
 
 
 # Runs the application
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
