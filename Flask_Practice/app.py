5a158d2abeff78fd0db47f94f537ea73a3a997a0
Sundeep Mistry <mistrysundeep@gmail.com>
2021-09-27 23:14:06 +0100

    Added html content
    Now working on getting the styling working

--- Flask_Practice/app.py
+++ Flask_Practice/app.py
@@ -1,7 +1,7 @@
 # This is mandatory for Flask projects
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="app")
 
 
 # .route() is used to the app which URL should call the associated function
